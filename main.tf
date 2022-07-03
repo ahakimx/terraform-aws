8490c3329b217572a7db018258eb3664ed654daa
ahakimx <ahakim338@gmail.com>
2022-07-03 22:05:31 +0700

    Added aws iam policy

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_iam_user" "admin_user" {
   }
 }
 
+resource "aws_iam_policy" "adminUser" {
+  name   = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "admin-access" {
+  user       = aws_iam_user.admin_user.name
+  policy_arn = aws_iam_policy.adminUser.arn
+}
