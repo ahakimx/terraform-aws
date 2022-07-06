ca36016bb4c7235bf042dcf82fe1cb4eb79eef22
ahakimx <ahakim338@gmail.com>
2022-07-06 20:10:10 +0700

    Added user login profile resource

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ resource "aws_iam_user" "admin_user" {
   }
 }
 
+resource "aws_iam_user_login_profile" "userLogin" {
+  user = aws_iam_user.admin_user.name
+}
+
 resource "aws_iam_policy" "adminUser" {
   name   = "AdminUsers"
   policy = file("admin-policy.json")
@@ -13,4 +17,8 @@ resource "aws_iam_policy" "adminUser" {
 resource "aws_iam_user_policy_attachment" "admin-access" {
   user       = aws_iam_user.admin_user.name
   policy_arn = aws_iam_policy.adminUser.arn
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.userLogin.password
 }
