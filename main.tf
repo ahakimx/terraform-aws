9aa473ba37acd4c8e8d7baa5ffbd89922256bcbc
ahakimx <ahakim338@gmail.com>
2022-07-07 17:59:01 +0700

    Added multiple users

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 resource "aws_iam_user" "admin_user" {
-  name = "ha"
+  name     = each.value
+  for_each = toset(var.admin_users)
   tags = {
     Description = "Tech Lead"
   }
 }
 
 resource "aws_iam_user_login_profile" "userLogin" {
-  user = aws_iam_user.admin_user.name
+  for_each = aws_iam_user.admin_user
+  user     = each.value.name
 }
 
 resource "aws_iam_policy" "adminUser" {
@@ -15,10 +17,13 @@ resource "aws_iam_policy" "adminUser" {
 }
 
 resource "aws_iam_user_policy_attachment" "admin-access" {
-  user       = aws_iam_user.admin_user.name
+  for_each   = aws_iam_user.admin_user
+  user       = each.value.name
   policy_arn = aws_iam_policy.adminUser.arn
 }
 
 output "password" {
-  value = aws_iam_user_login_profile.userLogin.password
+  value = {
+    for k, v in aws_iam_user_login_profile.userLogin : k => v.password
+  }
 }
---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "admin_users" {
+  type    = list(string)
+  default = ["ha", "lucy", "john"]
+}
