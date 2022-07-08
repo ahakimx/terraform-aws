4c849f9876764ca0f06b6c0dafb992875ab19911
ahakimx <ahakim338@gmail.com>
2022-07-08 15:31:46 +0700

    Added IAM group

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ resource "aws_iam_user" "admin_user" {
     Description = "Tech Lead"
   }
 }
+resource "aws_iam_group" "tech-lead" {
+  name = "Tech-Lead"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name     = "tech-lead-group-membership"
+  for_each = aws_iam_user.admin_user
+  users    = [each.value.name]
+  group    = aws_iam_group.tech-lead.name
+}
 
 resource "aws_iam_user_login_profile" "userLogin" {
   for_each = aws_iam_user.admin_user
@@ -16,9 +26,14 @@ resource "aws_iam_policy" "adminUser" {
   policy = file("admin-policy.json")
 }
 
-resource "aws_iam_user_policy_attachment" "admin-access" {
-  for_each   = aws_iam_user.admin_user
-  user       = each.value.name
+# resource "aws_iam_user_policy_attachment" "admin-access" {
+#   for_each   = aws_iam_user.admin_user
+#   user       = each.value.name
+#   policy_arn = aws_iam_policy.adminUser.arn
+# }
+
+resource "aws_iam_group_policy_attachment" "tech_lead_group_policy" {
+  group      = aws_iam_group.tech-lead.name
   policy_arn = aws_iam_policy.adminUser.arn
 }
 
