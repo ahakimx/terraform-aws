1dd32153325b18764abda63afad257051b8d792e
ahakimx <ahakim338@gmail.com>
2022-07-08 15:35:48 +0700

    deleted unnecessary resource

--- main.tf
+++ main.tf
@@ -26,12 +26,6 @@ resource "aws_iam_policy" "adminUser" {
   policy = file("admin-policy.json")
 }
 
-# resource "aws_iam_user_policy_attachment" "admin-access" {
-#   for_each   = aws_iam_user.admin_user
-#   user       = each.value.name
-#   policy_arn = aws_iam_policy.adminUser.arn
-# }
-
 resource "aws_iam_group_policy_attachment" "tech_lead_group_policy" {
   group      = aws_iam_group.tech-lead.name
   policy_arn = aws_iam_policy.adminUser.arn
