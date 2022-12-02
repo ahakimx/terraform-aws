95f571b92de23536a890e7eaf7d5a6cc43ec3ef9
ahakimx <ahakim338@gmail.com>
2022-12-02 19:30:46 +0700

    Updated main.tf & readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # terraform-aws
+
+### Overview
+This repo is a guide for provisioning AWS resources using Terraform.
+
+- IAM
+How to create IAM with AWS, you can use the below command:
+```bash
+cd iam
+terraform init
+terraform plan
+terraform apply
+```
+
+
+- Provisioning EC2 Instance
+```bash
+cd ec2
+terraform init
+terraform plan
+terraform apply
+```
+- Destroy resources
+```bash
+terraform destroy
+```
--- ec2/main.tf
+++ ec2/main.tf
@@ -1,35 +1,53 @@
 variable "availability_zone" {
-    default = "us-east-1a"
+  type = map(string)
+  default = {
+    az_a = "us-east-1a"
+    az_b = "us-east-1b"
+  }
 }
 
 variable "subnet" {
-    default = "subnet-0cb37cbbb41ee9aae"
+  type = map(string)
+  default = {
+    sn-web-A = "subnet-0cb37cbbb41ee9aae"
+    sn-web-B = "subnet-0fbe646eedf530a8b"
+  }
 }
 
 variable "ami" {
-    default = "ami-0b0dcb5067f052a63"
+  default = "ami-0b0dcb5067f052a63"
 }
 
 variable "instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "region" {
-    default = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "instance_count" {
-    default = "2"
+  default = "2"
 }
 
 resource "aws_instance" "ebs-instance-test" {
-    count             = var.instance_count
-    ami               = var.ami
-    instance_type     = var.instance_type
-    availability_zone = var.availability_zone
-    subnet_id         = var.subnet
+  count             = var.instance_count
+  ami               = var.ami
+  instance_type     = var.instance_type
+  availability_zone = var.availability_zone["az_a"]
+  subnet_id         = var.subnet["sn-web-A"]
+  tags = {
+    Name = "ebs-instance-test-${count.index}"
+  }
+}
 
-    tags = {
-        Name = "ebs-instance-test-${count.index}"
-    }
+resource "aws_instance" "ebs-instance-test-2" {
+  count             = 1
+  ami               = var.ami
+  instance_type     = var.instance_type
+  availability_zone = var.availability_zone["az_b"]
+  subnet_id         = var.subnet["sn-web-B"]
+  tags = {
+    Name = "ebs-instance-test-${count.index}"
+  }
 }
