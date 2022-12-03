3e6bf2746cfc728f21f53c396fb21f5c95edbd2f
ahakimx <ahakim338@gmail.com>
2022-12-03 16:32:32 +0700

    Updated EC2 script

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,53 +1,304 @@
-variable "availability_zone" {
-  type = map(string)
-  default = {
-    az_a = "us-east-1a"
-    az_b = "us-east-1b"
+#######################
+####### VPC ###########
+#######################
+resource "aws_vpc" "aha_vpc1" {
+  cidr_block                       = var.cidr_block
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  assign_generated_ipv6_cidr_block = true
+
+  tags = {
+    Name = "aha-vpc1"
   }
 }
 
-variable "subnet" {
-  type = map(string)
-  default = {
-    sn-web-A = "subnet-0cb37cbbb41ee9aae"
-    sn-web-B = "subnet-0fbe646eedf530a8b"
+##############################
+######## Subnet AZ-A #########
+##############################
+resource "aws_subnet" "sn_reserved_a" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_reserved_a
+  availability_zone               = var.az_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 00)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-reserved-A"
   }
 }
 
-variable "ami" {
-  default = "ami-0b0dcb5067f052a63"
+resource "aws_subnet" "sn_db_a" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_db_a
+  availability_zone               = var.az_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 01)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-db-A"
+  }
 }
 
-variable "instance_type" {
-  default = "t2.micro"
+resource "aws_subnet" "sn_app_a" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_app_a
+  availability_zone               = var.az_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 02)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-app-A"
+  }
 }
 
-variable "region" {
-  default = "us-east-1"
+resource "aws_subnet" "sn_web_a" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_web_a
+  availability_zone               = var.az_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 03)
+  assign_ipv6_address_on_creation = true
+  map_public_ip_on_launch         = true
+
+  tags = {
+    "Name" = "sn-web-A"
+  }
 }
 
-variable "instance_count" {
-  default = "2"
+##############################
+######## Subnet AZ-B #########
+##############################
+resource "aws_subnet" "sn_reserved_b" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_reserved_b
+  availability_zone               = var.az_b
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 04)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-reserved-B"
+  }
 }
 
+resource "aws_subnet" "sn_db_b" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_db_b
+  availability_zone               = var.az_b
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 05)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-db-B"
+  }
+}
+
+resource "aws_subnet" "sn_app_b" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_app_b
+  availability_zone               = var.az_b
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 06)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-app-B"
+  }
+}
+
+resource "aws_subnet" "sn_web_b" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_web_b
+  availability_zone               = var.az_b
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 07)
+  assign_ipv6_address_on_creation = true
+  map_public_ip_on_launch         = true
+
+  tags = {
+    "Name" = "sn-web-B"
+  }
+}
+
+##############################
+######## Subnet AZ-C #########
+##############################
+resource "aws_subnet" "sn_reserved_c" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_reserved_c
+  availability_zone               = var.az_c
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 08)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-reserved-C"
+  }
+}
+
+resource "aws_subnet" "sn_db_c" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_db_c
+  availability_zone               = var.az_c
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 09)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-db-C"
+  }
+}
+
+resource "aws_subnet" "sn_app_c" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_app_c
+  availability_zone               = var.az_c
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 10)
+  assign_ipv6_address_on_creation = true
+
+  tags = {
+    "Name" = "sn-app-C"
+  }
+}
+
+resource "aws_subnet" "sn_web_c" {
+  vpc_id                          = aws_vpc.aha_vpc1.id
+  cidr_block                      = var.sn_web_c
+  availability_zone               = var.az_c
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.aha_vpc1.ipv6_cidr_block, 8, 11)
+  assign_ipv6_address_on_creation = true
+  map_public_ip_on_launch         = true
+
+  tags = {
+    "Name" = "sn-web-C"
+  }
+}
+
+###################################
+######## Internet Gateway #########
+###################################
+
+resource "aws_internet_gateway" "aha_vpc1_igw" {
+  vpc_id = aws_vpc.aha_vpc1.id
+
+  tags = {
+    Name = "aha-vpc1-igw"
+  }
+}
+
+################################
+######## Route Table ###########
+################################
+resource "aws_route_table" "aha_vpc1_rt_table" {
+  vpc_id = aws_vpc.aha_vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.aha_vpc1_igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.aha_vpc1_igw.id
+  }
+  tags = {
+    Name = "aha-vpc1-rt-web"
+  }
+}
+
+############################################
+######## Route Table Association ###########
+############################################
+resource "aws_route_table_association" "rta_web_a" {
+  subnet_id      = aws_subnet.sn_web_a.id
+  route_table_id = aws_route_table.aha_vpc1_rt_table.id
+}
+
+resource "aws_route_table_association" "rta_web_b" {
+  subnet_id      = aws_subnet.sn_web_b.id
+  route_table_id = aws_route_table.aha_vpc1_rt_table.id
+}
+
+resource "aws_route_table_association" "rta_web_c" {
+  subnet_id      = aws_subnet.sn_web_c.id
+  route_table_id = aws_route_table.aha_vpc1_rt_table.id
+}
+
+#############################
+######## Key Pair ###########
+#############################
+resource "tls_private_key" "aha_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "aha_key_pair" {
+  key_name   = var.key_pair
+  public_key = tls_private_key.aha_private_key.public_key_openssh
+
+}
+
+#########################################################
+######## EC2 Instance - EBS Instance Test 0-1 ###########
+#########################################################
 resource "aws_instance" "ebs-instance-test" {
   count             = var.instance_count
   ami               = var.ami
   instance_type     = var.instance_type
-  availability_zone = var.availability_zone["az_a"]
-  subnet_id         = var.subnet["sn-web-A"]
+  availability_zone = var.az_a
+  subnet_id         = aws_subnet.sn_web_a.id
+  key_name = aws_key_pair.aha_key_pair.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.aha_sg.id
+  ]
   tags = {
     Name = "ebs-instance-test-${count.index}"
   }
 }
 
+#########################################################
+######## EC2 Instance - EBS Instance Test 2 ###########
+#########################################################
 resource "aws_instance" "ebs-instance-test-2" {
-  count             = 1
   ami               = var.ami
   instance_type     = var.instance_type
-  availability_zone = var.availability_zone["az_b"]
-  subnet_id         = var.subnet["sn-web-B"]
+  availability_zone = var.az_b
+  subnet_id         = aws_subnet.sn_web_b.id
+  key_name = aws_key_pair.aha_key_pair.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.aha_sg.id
+  ]
+
   tags = {
-    Name = "ebs-instance-test-${count.index}"
+    Name = "ebs-instance-test-2"
   }
 }
+
+####################################
+######## Security Group ############
+####################################
+resource "aws_security_group" "aha_sg" {
+  name   = "aha-sg"
+  vpc_id = aws_vpc.aha_vpc1.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+############################
+######## Output ############
+############################
+output "private_key" {
+  value     = tls_private_key.aha_private_key.private_key_pem
+  sensitive = true
+}
+
+## to see private key pem output, use the command below
+## terraform output -raw private_key
---
+++ ec2/variable.tf
@@ -0,0 +1,80 @@
+variable "cidr_block" {
+  default = "10.16.0.0/16"
+
+}
+
+variable "az_a" {
+  default = "us-east-1a"
+}
+
+variable "az_b" {
+  default = "us-east-1b"
+}
+
+variable "az_c" {
+  default = "us-east-1c"
+}
+
+variable "sn_reserved_a" {
+  default = "10.16.0.0/20"
+}
+
+variable "sn_db_a" {
+  default = "10.16.16.0/20"
+}
+
+variable "sn_app_a" {
+  default = "10.16.32.0/20"
+}
+
+variable "sn_web_a" {
+  default = "10.16.48.0/20"
+}
+
+variable "sn_reserved_b" {
+  default = "10.16.64.0/20"
+}
+
+variable "sn_db_b" {
+  default = "10.16.80.0/20"
+}
+
+variable "sn_app_b" {
+  default = "10.16.96.0/20"
+}
+
+variable "sn_web_b" {
+  default = "10.16.112.0/20"
+}
+
+variable "sn_reserved_c" {
+  default = "10.16.128.0/20"
+}
+
+variable "sn_db_c" {
+  default = "10.16.144.0/20"
+}
+
+variable "sn_app_c" {
+  default = "10.16.160.0/20"
+}
+
+variable "sn_web_c" {
+  default = "10.16.176.0/20"
+}
+
+variable "ami" {
+  default = "ami-0b0dcb5067f052a63"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_pair" {
+  default = "aha-key-pair"
+}
+
+variable "instance_count" {
+  default = 2
+}
