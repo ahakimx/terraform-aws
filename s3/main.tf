ca3929e2223f98b740c83c2219e2e7da83490a12
ahakimx <ahakim338@gmail.com>
2022-12-24 00:05:14 +0700

    Updated filename to index.html & added public policy to bucket

--- s3/main.tf
+++ s3/main.tf
@@ -1,3 +1,4 @@
+## create s3 bucket
 resource "aws_s3_bucket" "aha_bucket" {
   bucket = "aha-test-12324123"
 
@@ -7,9 +8,21 @@ resource "aws_s3_bucket" "aha_bucket" {
   }
 }
 
+
+## create public policies
+resource "aws_s3_bucket_public_access_block" "public_policy" {
+    bucket = aws_s3_bucket.aha_bucket.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+## upload object to bucket
 resource "aws_s3_bucket_object" "aha_object" {
     bucket = aws_s3_bucket.aha_bucket.bucket
-    key =  "test_file.txt"
-    source = "test_file.txt"
+    key =  "index.html"
+    source = "index.html"
   
 }
